81892f9909b339f8ce51e021071122da85df27b0
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-03-04 10:05:23 -0300

    Deleted implemented

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,16 +1,31 @@
 /**
  * Created by lucas on 3/4/17.
  */
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
-import {fetchPost} from '../actions/index';
+import {fetchPost, deletePost} from '../actions/index';
 import {Link} from 'react-router';
 
 class PostsShow extends Component {
 
+    // To get access to the push property of React Router
+    // Can get information from parent components
+    static contextTypes = {
+        router: PropTypes.object
+    };
+
+
     componentWillMount() {
         this.props.fetchPost(this.props.params.id);
     }
+
+    onDeleteClick = () => {
+        this.props.deletePost(this.props.params.id)
+            .then(() => {
+                // blog post successfully deleted. Navigate to index
+                this.context.router.push('/');
+            });
+    };
 
     render() {
 
@@ -21,7 +36,12 @@ class PostsShow extends Component {
         }
         return (
             <div>
-                <Link to="/" className="btn btn-primary">Back</Link>
+                <Link to="/">Back to list</Link>
+                <button
+                    onClick={this.onDeleteClick}
+                    className="btn btn-danger pull-xs-right">
+                    Delete post
+                </button>
                 <h3>{post.title}</h3>
                 <h6>{post.categories}</h6>
                 <p>{post.content}</p>
@@ -36,4 +56,4 @@ function mapStateToProps(state) {
     }
 }
 
-export default connect(mapStateToProps, {fetchPost})(PostsShow);
+export default connect(mapStateToProps, {deletePost, fetchPost})(PostsShow);
