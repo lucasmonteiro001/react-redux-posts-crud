04a25b2f535f8cff57637cadd7859b73d5681d88
Lucas Monteiro <lucasmonteiro001@gmail.com>
2017-03-04 15:49:40 -0300

    Redux DevTools added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import promise from 'redux-promise';
 import {Router, browserHistory} from 'react-router';
 import reducers from './reducers';
-
+import thunk from 'redux-thunk';
 import routes from './routes';
 
+// OLD WAY OF CREATING STORE
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
+const middlewares = applyMiddleware(thunk, promise)
+
+const enhancer = compose(middlewares, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const store = createStore(reducers, enhancer);
+
+{/*<Provider store={createStoreWithMiddleware(reducers)}>*/
+}
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-      <Router history={browserHistory} routes={routes}/>
-  </Provider>
-  , document.querySelector('.container'));
+    <Provider store={store}>
+        <div>
+            <Router history={browserHistory} routes={routes}/>
+        </div>
+    </Provider>
+    , document.querySelector('.container'));
